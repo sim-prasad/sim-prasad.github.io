473cf661cfe8065cbc612ef634fc769c08ced341
sim-prasad <spra72@student.monash.edu>
2018-06-01 16:23:00 +1000

    swotvac

--- 8_reader/assets/js/main.js
+++ 8_reader/assets/js/main.js
@@ -1,13 +1,45 @@
 $(document).ready(function() {
 
 
-// Add jQuery here
+  $('.r-fashion').mouseenter(function() {
+     $('.p-fashion').addClass('p-show');
+   });
+
+ $('.r-fashion').mouseleave(function() {
+     $('.p-fashion').removeClass('p-show');
+   });
+
+
+
+  $('.r-language').mouseenter(function() {
+     $('.p-language').addClass('p-show');
+   });
+
+ $('.r-language').mouseleave(function() {
+     $('.p-language').removeClass('p-show');
+   });
 
 
 
 
+   $('.r-religion').mouseenter(function() {
+      $('.p-religion').addClass('p-show');
+    });
+
+   $('.r-religion').mouseleave(function() {
+      $('.p-religion').removeClass('p-show');
+    });
 
 
+
+
+    $('.r-sport').mouseenter(function() {
+       $('.p-sport').addClass('p-show');
+     });
+
+    $('.r-sport').mouseleave(function() {
+       $('.p-sport').removeClass('p-show');
+     });
 
 
 
