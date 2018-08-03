633d18e8c52b97c0b5c326ea66c568a21abdca7a
sim-prasad <spra72@student.monash.edu>
2018-08-03 17:05:23 +1000

    3 aug

---
+++ 0_sem1/8_reader/assets/js/main.js
@@ -0,0 +1,259 @@
+$(document).ready(function() {
+
+
+  $('.m-frog').mouseenter(function() {
+     $('.c-frog').addClass('c-show');
+   });
+
+ $('.m-frog').mouseleave(function() {
+     $('.c-frog').removeClass('c-show');
+   });
+
+
+
+  $('.m-bear').mouseenter(function() {
+     $('.c-bear').addClass('c-show');
+   });
+
+ $('.m-bear').mouseleave(function() {
+     $('.c-bear').removeClass('c-show');
+   });
+
+
+
+   $('.m-lion').mouseenter(function() {
+      $('.c-lion').addClass('c-show');
+    });
+
+  $('.m-lion').mouseleave(function() {
+      $('.c-lion').removeClass('c-show');
+    });
+
+
+
+
+    $('.m-froggy').mouseenter(function() {
+       $('.c-froggy').addClass('c-show');
+     });
+
+   $('.m-froggy').mouseleave(function() {
+       $('.c-froggy').removeClass('c-show');
+     });
+
+
+
+     $('.m-nose').mouseenter(function() {
+        $('.c-nose').addClass('c-show');
+      });
+
+    $('.m-nose').mouseleave(function() {
+        $('.c-nose').removeClass('c-show');
+      });
+
+
+
+    $('.m-cat').mouseenter(function() {
+        $('.c-cat').addClass('c-show');
+      });
+
+      $('.m-cat').mouseleave(function() {
+        $('.c-cat').removeClass('c-show');
+      });
+
+
+
+      $('.m-homer').mouseenter(function() {
+          $('.c-homer').addClass('c-show');
+        });
+
+        $('.m-homer').mouseleave(function() {
+          $('.c-homer').removeClass('c-show');
+        });
+
+
+
+
+        $('.m-girl').mouseenter(function() {
+            $('.c-girl').addClass('c-show');
+          });
+
+          $('.m-girl').mouseleave(function() {
+            $('.c-girl').removeClass('c-show');
+          });
+
+
+          $('.m-old').mouseenter(function() {
+              $('.c-old').addClass('c-show');
+            });
+
+            $('.m-old').mouseleave(function() {
+              $('.c-old').removeClass('c-show');
+            });
+
+
+
+          $('.m-salt').mouseenter(function() {
+              $('.c-salt').addClass('c-show');
+            });
+
+          $('.m-salt').mouseleave(function() {
+              $('.c-salt').removeClass('c-show');
+            });
+
+
+
+            $('.m-sponge').mouseenter(function() {
+                $('.c-sponge').addClass('c-show');
+              });
+
+            $('.m-sponge').mouseleave(function() {
+                $('.c-sponge').removeClass('c-show');
+              });
+
+
+
+              $('.m-barbie').mouseenter(function() {
+                  $('.c-barbie').addClass('c-show');
+                });
+
+              $('.m-barbie').mouseleave(function() {
+                  $('.c-barbie').removeClass('c-show');
+                });
+
+
+
+              $('.m-thinking').mouseenter(function() {
+                    $('.c-thinking').addClass('c-show');
+                  });
+
+                $('.m-thinking').mouseleave(function() {
+                    $('.c-thinking').removeClass('c-show');
+                  });
+
+
+
+              $('.m-dancing').mouseenter(function() {
+              $('.c-dancing').addClass('c-show');
+              });
+
+              $('.m-dancing').mouseleave(function() {
+                  $('.c-dancing').removeClass('c-show');
+                });
+
+
+                  $('.m-elmo').mouseenter(function() {
+                  $('.c-elmo').addClass('c-show');
+                  });
+
+                  $('.m-elmo').mouseleave(function() {
+                      $('.c-elmo').removeClass('c-show');
+                    });
+
+
+
+                $('.m-sponges').mouseenter(function() {
+                $('.c-sponges').addClass('c-show');
+                  });
+
+                  $('.m-sponges').mouseleave(function() {
+                    $('.c-sponges').removeClass('c-show');
+                });
+
+
+
+                $('.m-flat').mouseenter(function() {
+                $('.c-flat').addClass('c-show');
+                  });
+
+                  $('.m-flat').mouseleave(function() {
+                    $('.c-flat').removeClass('c-show');
+                });
+
+
+
+                $('.m-door').mouseenter(function() {
+                $('.c-door').addClass('c-show');
+                  });
+
+                  $('.m-door').mouseleave(function() {
+                    $('.c-door').removeClass('c-show');
+                });
+
+
+
+
+                $('.m-tea').mouseenter(function() {
+                $('.c-tea').addClass('c-show');
+                  });
+
+                  $('.m-tea').mouseleave(function() {
+                    $('.c-tea').removeClass('c-show');
+                });
+
+
+
+                $('.m-pitbull').mouseenter(function() {
+                $('.c-pitbull').addClass('c-show');
+                  });
+
+                  $('.m-pitbull').mouseleave(function() {
+                    $('.c-pitbull').removeClass('c-show');
+                });
+
+
+
+                $('.m-poo').mouseenter(function() {
+                $('.c-poo').addClass('c-show');
+                  });
+
+                  $('.m-poo').mouseleave(function() {
+                    $('.c-poo').removeClass('c-show');
+                });
+
+
+
+
+                $('.m-scream').mouseenter(function() {
+                $('.c-scream').addClass('c-show');
+                  });
+
+                  $('.m-scream').mouseleave(function() {
+                    $('.c-scream').removeClass('c-show');
+                });
+
+
+
+
+                $('.m-patrick').mouseenter(function() {
+                $('.c-patrick').addClass('c-show');
+                  });
+
+                  $('.m-patrick').mouseleave(function() {
+                    $('.c-patrick').removeClass('c-show');
+                });
+
+
+
+
+                $('.m-bruh').mouseenter(function() {
+                $('.c-bruh').addClass('c-show');
+                  });
+
+                  $('.m-bruh').mouseleave(function() {
+                    $('.c-bruh').removeClass('c-show');
+                });
+
+
+
+                $('.m-smile').mouseenter(function() {
+                $('.c-smile').addClass('c-show');
+                  });
+
+                  $('.m-smile').mouseleave(function() {
+                    $('.c-smile').removeClass('c-show');
+                });
+
+
+
+// dont delete this
+ });
